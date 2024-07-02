10102a721d641a6c2ada6b1c551d0bd1f8660a9d
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-07-02 18:02:15 +0500

    practice

---
+++ 01_helloworld_string/index.py
@@ -0,0 +1,7 @@
+name: str = "Sarfaraz"
+print(name)  # print
+print(type(name))  # type
+print(id(name))  # physcial address
+
+name = 20  # Expression of type "Literal[20]" is incompatible with declared type "str"
+print(name)
