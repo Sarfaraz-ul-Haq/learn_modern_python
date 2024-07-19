66c73863ee17c0afe2ef8bfff98799906aff48fc
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-07-19 22:30:58 +0500

    practice

---
+++ 09_loop_input/index.py
@@ -0,0 +1,10 @@
+import sys
+
+# arguments: list = sys.argv
+
+# print("Script Name:", arguments[0])
+# print("First Argument:", arguments[1])
+# print("Second Argument:", arguments[2])
+# print("Third Argument:", arguments[3])
+
+print(sys.argv)
